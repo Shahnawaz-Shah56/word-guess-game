b8918181958b01537a6d5ea12cf366eb682b070b
Shahnawaz Shah <shahcreation6158@gmail.com>
2026-06-01 18:38:54 +0530

    Fix: Restore Home component and update project

--- src/components/Start.wordguess..jsx
+++ src/components/Start.wordguess..jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const Start = ({onPlaying}) => {
+const Start = ({ onPlaying }) => {
   return (
     <div>
       <button onClick={onPlaying}>Play Now</button>
     </div>
   )
+}
+
+Start.propTypes = {
+  onPlaying: PropTypes.func.isRequired
 }
 
 export default Start
